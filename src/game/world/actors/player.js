53ccd53cd7085cd033bac21063c307e199d7f8d8
JotaSe <jota.segovia@gmail.com>
2014-04-26 00:34:25 -0430

    Added behavior to cardumen

--- src/game/world/actors/player.js
+++ src/game/world/actors/player.js
@@ -3,7 +3,8 @@ game.module(
 )
     .require(
         'engine.physics',
-        'plugins.AI'
+        'plugins.AI',
+        'engine.sprite'
 )
     .body(function () {
 
@@ -16,7 +17,45 @@ game.module(
                 this.AI = new game.SteeringBehavior();
 
 
-            }
+            },
+            collide: function () {
+                // console.log("colision bot");
+                game.scene.world.removeBody(this);
+                game.scene.cardumenContainer.removeChild(this.sprite);
+                game.scene.removeObject(this);
+
+
+                // game.scene.interactive.addScore(this.value);
+
+                game.scene.world.removeBodyCollision(this.body);
+
+            },
+            update: function () {
+                this.flip();
+                // console.log("velocity y: " + this.body.velocity.y);
+                this.sprite.position.x = this.body.position.x;
+                this.sprite.position.y = this.body.position.y;
+                if (this.sprite.position.y < 0) {
+
+                    game.scene.world.removeBody(this.body);
+                    game.scene.ObjectsContainer.removeChild(this.sprite);
+                    game.scene.removeObject(this);
+
+                }
+
+
+            },
+            flip: function () {
+                if (this.body.velocity.x > 0) {
+                    this.sprite.scale.x = -1;
+
+                } else {
+                    if (this.body.velocity.x < 0) {
+                        this.sprite.scale.x = -1;
+                    }
+                }
+
+            },
 
 
         })

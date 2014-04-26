9048b53d6fd4cd3812b684d2dd5d49d0df100467
JotaSe <jota.segovia@gmail.com>
2014-04-26 00:23:17 -0430

    AS

--- src/game/world/actors/player.js
+++ src/game/world/actors/player.js
@@ -7,16 +7,17 @@ game.module(
 )
     .body(function () {
 
-            Cardumen = game.Class.extend({
-                _speed: 50,
-                init: function () {
-                    var x = Math.random() * (game.system.width);
-                    var y = Math.random() * (game.system.width - 100) + 100;
-                    this.sprite = new game.Sprite(160, 32, 'media/images/cardumentest.png');
-                    this.AI = new game.SteeringBehavior();
+        Cardumen = game.Class.extend({
+            _speed: 50,
+            init: function () {
+                var x = Math.random() * (game.system.width);
+                var y = Math.random() * (game.system.width - 100) + 100;
+                this.sprite = new game.Sprite(160, 32, 'media/images/cardumentest.png');
+                this.AI = new game.SteeringBehavior();
 
 
-                }
+            }
 
 
-            })
+        })
+    });
